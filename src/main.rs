fca6ea96b5b32babb33487eb1ae0b2fb623e3b11
Martin Nordholts <martin.nordholts@codetale.se>
2026-02-19 19:53:12 +0100

    Only care about MemAvailable, and flesh out README

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,25 @@ fn main() {
     let mut prev_cpu_times = cpu_times::snapshot();
     let baseline_mem_info = mem_info::snapshot();
 
-    println!("ms	CPU%	Avai kB	Free kB");
+    println!("ms	CPU‰	Avail kB");
     let start_time = std::time::SystemTime::now();
     loop {
+        // Sleep
         std::thread::sleep(std::time::Duration::from_millis(args.interval_millis));
 
+        // Sample CPU
         let current_cpu_times = cpu_times::snapshot();
         let cpu_usage_percentage = current_cpu_times.usage_percentage_since(&prev_cpu_times);
         prev_cpu_times = current_cpu_times;
 
+        // Sample Memory
         let mem_info_delta = mem_info::snapshot() - baseline_mem_info;
 
+        // Print
         let elapsed = start_time.elapsed().unwrap().as_millis();
         println!(
-            "{elapsed}	{cpu_usage_percentage:.2}	{}	{}",
-            mem_info_delta.available_kb, mem_info_delta.free_kb,
+            "{elapsed}	{cpu_usage_percentage}	{available}",
+            available = mem_info_delta.available_kb,
         );
     }
 }
