8a4b915a3857f17a5d403c413f75a5e39b6b1f1f
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-03 18:57:22 +0100

    wip  2026-03-03T18:57:22+01:00

--- src/args.rs
+++ src/args.rs
@@ -1,53 +1,15 @@
+#[derive(argh::FromArgs)]
+/// Sample CPU and memory usage at fixed intervals.
 pub struct Args {
+    #[argh(option, default = "1000")]
+    /// interval between samples in milliseconds.
     pub interval_millis: u64,
+
+    #[argh(option)]
+    /// optional file path for an auxiliary data point displayed as the final column.
     pub aux_data_point_path: Option<String>,
 }
 
-impl Default for Args {
-    fn default() -> Self {
-        Self {
-            interval_millis: 1000,
-            aux_data_point_path: None,
-        }
-    }
-}
-
 pub fn parse() -> Args {
-    enum State {
-        ExpectingArg,
-        ExpectingIntervalMillisValue,
-        ExpectingAuxDataPointPathValue,
-    }
-    let mut args = Args::default();
-
-    let mut state = State::ExpectingArg;
-    for arg in std::env::args().skip(1) {
-        match state {
-            State::ExpectingIntervalMillisValue => {
-                args.interval_millis = arg.parse::<u64>().unwrap_or_else(|_| {
-                    super::error_exit(&format!(
-                        "Failed to parse --interval-millis argument `{}`",
-                        arg
-                    ));
-                });
-                state = State::ExpectingArg;
-            }
-            State::ExpectingAuxDataPointPathValue => {
-                args.aux_data_point_path = Some(arg);
-                state = State::ExpectingArg;
-            }
-            State::ExpectingArg => {
-                if arg == "--interval-millis" {
-                    state = State::ExpectingIntervalMillisValue;
-                    continue;
-                } else if arg == "--aux-data-point-path" {
-                    state = State::ExpectingAuxDataPointPathValue;
-                    continue;
-                } else {
-                    super::error_exit(&format!("Unknown argument `{}`", arg));
-                }
-            }
-        }
-    }
-    args
+    argh::from_env()
 }
